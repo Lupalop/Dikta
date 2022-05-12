668fea725c21566a756e25fb09effd14ed81e22b
Francis Fajardo <francisdominic.fajardo@gmail.com>
2022-05-12 18:23:26 +0800

    Add debug coordinates overlay on startup

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,6 +1,7 @@
 from engine import *
-from app.scenes.main_menu import MainMenuScene
+from app.scenes import *
 
+game.scenes.add_overlay("dbg_coords", CoordinatesDebugOverlay())
 game.scenes.set_scene(MainMenuScene())
 game.set_window_title("Dikta")
 game.run()
