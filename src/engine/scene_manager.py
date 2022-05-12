239d943c0320980a81c1570ed5c4836e1f8e6c57
Francis Fajardo <francisdominic.fajardo@gmail.com>
2022-05-12 18:23:08 +0800

    Add support for overlays in the scene manager

--- src/engine/scene_manager.py
+++ src/engine/scene_manager.py
@@ -2,6 +2,7 @@ class SceneManager:
     def __init__(self):
         print("Initialized: Scene Manager")
         self._scene = None
+        self._overlays = {}
 
     def get_scene(self):
         return self._scene
@@ -13,14 +14,28 @@ class SceneManager:
         self._scene = scene
         scene.load_content()
 
-    def update(self, events):
-        if not self._scene or not self._scene.enabled:
-            return
+    def add_overlay(self, id, scene):
+        scene.load_content()
+        self._overlays[id] = scene
 
-        self._scene.update(events)
+    def remove_overlay(self, id):
+        return self._overlays.pop(id, None)
+
+    def get_overlay(self, id):
+        return self._overlays.get(id, None)
+
+    def update(self, events):
+        if self._scene and self._scene.enabled:
+            self._scene.update(events)
+
+        for overlay in self._overlays.values():
+            if not overlay.enabled:
+                continue
+            overlay.update(events)
 
     def draw(self, window):
-        if not self._scene:
-            return
+        if self._scene:
+            self._scene.draw(window)
 
-        self._scene.draw(window)
+        for overlay in self._overlays.values():
+            overlay.draw(window)
