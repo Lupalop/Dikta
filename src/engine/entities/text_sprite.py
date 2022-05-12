190852c96c5b43ac7896e9756ef65d95de9bd55e
Francis Fajardo <francisdominic.fajardo@gmail.com>
2022-05-12 13:52:00 +0800

    Add mask creation to sprite classes

--- src/engine/entities/text_sprite.py
+++ src/engine/entities/text_sprite.py
@@ -9,11 +9,15 @@ class TextSprite():
 
     def _update_texture(self, compute_size = True):
         self._texture = self._font.render(self._text, True, self._color, None)
+        self._mask = pygame.mask.from_surface(self._texture)
         if compute_size:
             self._size = self._font.size(self._text)
 
     def get_texture(self):
         return self._texture
+
+    def get_mask(self):
+        return self._mask
 
     def set_text(self, text):
         self._text = text
--- src/engine/entities/texture_sprite.py
+++ src/engine/entities/texture_sprite.py
@@ -12,8 +12,15 @@ class TextureSprite():
     def set_texture(self, texture):
         self._texture_base = texture
         self._texture = texture
+        self._update_mask()
         self._size_base = texture.get_rect()
         self._size = texture.get_rect()
+
+    def _update_mask(self):
+        self._mask = pygame.mask.from_surface(self._texture)
+
+    def get_mask(self):
+        return self._mask
 
     def get_size(self):
         return self._size
@@ -21,7 +28,9 @@ class TextureSprite():
     def set_size(self, size):
         self._size = size
         self._texture = pygame.transform.scale(self._texture_base, size)
+        self._update_mask()
 
     def reset(self):
         self._texture = self._texture_base
         self._size = self._size_base
+        self._update_mask()
