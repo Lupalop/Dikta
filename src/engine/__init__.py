a993bb5c9d3258a07abf9e049be5dd7edfece1a7
Francis Fajardo <francisdominic.fajardo@gmail.com>
2022-05-13 17:39:03 +0800

    De-staticify the content manager

--- src/engine/__init__.py
+++ src/engine/__init__.py
@@ -1,12 +1,11 @@
 from . import entities
+from . import content
 
 from .scene import Scene
 
-from .content_manager import ContentManager
 from .scene_manager import SceneManager
 from .game_manager import GameManager
 
-content = ContentManager()
 game = GameManager()
 
 # TODO: Move these constants to their own module (e.g., utils.py)
