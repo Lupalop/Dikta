5ba29b7644eed987598557a4f96fcfab8cace593
Francis Fajardo <francisdominic.fajardo@gmail.com>
2022-05-13 17:30:16 +0800

    Use default small font in debug coordinates overlay

--- src/app/scenes/dbg_coords.py
+++ src/app/scenes/dbg_coords.py
@@ -1,5 +1,6 @@
 from engine import *
 from engine.entities import *
+from app.utils import *
 
 import pygame
 
@@ -14,8 +15,7 @@ class CoordinatesDebugOverlay(Scene):
         self.entities["xy"].set_text(scaled_pos)
 
     def load_content(self):
-        counter_font = content.load_font("arial", 12)
-        xy_counter = Label("0, 0", counter_font, pygame.Color("white"), (0, 0))
+        xy_counter = Label("0, 0", get_font("sm"), pygame.Color("white"), (0, 0))
 
         self.entities = {
             "xy": xy_counter
