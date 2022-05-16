a57b5602c090aa89e3b1293dfd1cb00cdf1da940
Francis Fajardo <francisdominic.fajardo@gmail.com>
2022-05-16 14:58:39 +0800

    Initial main menu update

--- src/app/scenes/main_menu.py
+++ src/app/scenes/main_menu.py
@@ -1,5 +1,7 @@
 from engine import *
 from engine.entities import *
+from app import utils
+from app.scenes import *
 
 import pygame
 
@@ -7,28 +9,33 @@ class MainMenuScene(Scene):
     def __init__(self):
         super().__init__("Main Menu")
 
-    def update(self, events):
-        super().update(events)
+    def update(self, game, events):
+        super().update(game, events)
 
-        for event in events:
-            if event.type == pygame.KEYDOWN and event.key == pygame.K_x:
-                print('x pressed')
-
-    def draw(self, window):
-        super().draw(window)
+    def draw(self, layer):
+        super().draw(layer)
 
     def load_content(self):
         background = Image(
-            content.load_asset('mainBG.png'), (0, 0))
+            content.load_image("bg-main-menu.png"), (0, 0))
         logo = Image(
-            content.load_asset('logoName.png'), (70, 80), (300, 80))
-        menu_font = content.load_font('ocraextended', 20)
-        landing_ng_text = Label(
-            "PRESS X FOR NEW GAME", menu_font,
-            pygame.Color("white"), (500, 110))
+            content.load_image("game-logo.png"), (70, 80))
+        label_version = Label(
+            "Version 1.0.0a1 - For testing purposes only.", utils.fonts["sm"], pygame.Color("white"), (35, 555))
+        label_copyright = Label(
+            "© 2022 Pacific Tech", utils.fonts["sm"], pygame.Color("white"), (35, 565))
         
+        button_new_game = Button.fromButton(utils.button_default, "New Game")
+        button_new_game.set_position((510, 100))
+        def on_new_game_click():
+            pass
+            #game.scenes.set_scene(
+        button_new_game.on_left_click = on_new_game_click
+
         self.entities = {
-            "bg": background,
+            "background": background,
             "logo": logo,
-            "landing_ng_text": landing_ng_text
+            "label_copyright": label_copyright,
+            "label_version": label_version,
+            "button_new_game": button_new_game,
         }
