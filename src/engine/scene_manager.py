a18604a3868c8c2cca1bea8f3bb44b98a3bc5b77
Francis Fajardo <francisdominic.fajardo@gmail.com>
2022-06-01 02:18:42 +0800

    Implement fading when switching between scenes

--- src/engine/scene_manager.py
+++ src/engine/scene_manager.py
@@ -1,9 +1,16 @@
+from engine.timer import Timer
+
+import pygame
+
 class SceneManager:
     def __init__(self):
         print("Initialized: Scene Manager")
         self.all_scenes = {}
         self._scene = None
         self._overlays = {}
+        self.fade_surface = pygame.Surface((1360, 765)) # FIXME: Resolution should not be hardcoded
+        self.fade_surface.fill(pygame.Color("black"))
+        self.fade_surface.set_alpha(255)
 
     def get_scene(self):
         return self._scene
@@ -17,11 +24,30 @@ class SceneManager:
             else:
                 return
 
+        # TODO: Should be in a utility/animation/transition module
+        def _fade_to_target(target_alpha):
+            alpha = self.fade_surface.get_alpha()
+            time_ratio = self.fade_timer.get_elapsed() / self.fade_timer.interval
+            alpha += ((target_alpha - alpha) * time_ratio)
+            self.fade_surface.set_alpha(alpha)
+
+        def _fade_in_done():
+            pending_scene.load_content()
+            self._scene = pending_scene
+
+            self.fade_timer = Timer(1000)
+            self.fade_timer.tick += lambda: _fade_to_target(0)
+            self.fade_timer.start()
+
         if self._scene:
             self._scene.dispose()
 
-        self._scene = pending_scene
-        pending_scene.load_content()
+            self.fade_timer = Timer(1000)
+            self.fade_timer.tick += lambda: _fade_to_target(255)
+            self.fade_timer.elapsed += _fade_in_done
+            self.fade_timer.start()
+        else:
+            _fade_in_done()
 
     def add_overlay(self, id, scene):
         scene.load_content()
@@ -48,3 +74,5 @@ class SceneManager:
 
         for overlay in self._overlays.values():
             overlay.draw(layer)
+
+        layer.blit(self.fade_surface, (0, 0))
