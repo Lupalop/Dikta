6a624b89ed2459750a5d071e64c472db7e35124d
Francis Fajardo <francisdominic.fajardo@gmail.com>
2022-06-01 03:18:32 +0800

    Add Entity variant of `to_alpha`

--- src/engine/animator.py
+++ src/engine/animator.py
@@ -5,3 +5,6 @@ def to_alpha(surface, target_alpha, timer):
     time_ratio = timer.get_elapsed() / timer.interval
     alpha += ((target_alpha - alpha) * time_ratio)
     surface.set_alpha(alpha)
+
+def entity_to_alpha(entity, target_alpha, timer):
+    to_alpha(entity.get_surface(), target_alpha, timer)
