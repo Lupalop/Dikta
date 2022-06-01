1c7faac4bb25034a80c334b6f2f45368d67ab38f
Francis Fajardo <francisdominic.fajardo@gmail.com>
2022-06-01 17:57:44 +0800

    Suppress PyGame console message

--- src/__init__.py
+++ src/__init__.py
@@ -1,2 +1,11 @@
+# MaquinaPy Game Runner
+
+# The following suppresses the PyGame message emitted in the console
+# on startup.
+from os import environ
+environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "1"
+import pygame
+
+# Initialize engine and app packages.
 import engine
 import app
