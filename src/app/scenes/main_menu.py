3d6cb1a85ff08a02e5698fdc1255d6aef6896049
Francis Fajardo <francisdominic.fajardo@gmail.com>
2022-06-01 09:29:01 +0800

    Land revamped main menu

--- src/app/scenes/main_menu.py
+++ src/app/scenes/main_menu.py
@@ -1,7 +1,6 @@
 from engine import *
 from engine.entities import *
 from app import utils
-from app.scenes import *
 
 import pygame
 
@@ -17,32 +16,48 @@ class MainMenuScene(Scene):
 
     def load_content(self):
         background = Image(
-            content.load_image("bg-main-menu.png"), (0, 0))
+            utils.load_mm_image("bg-main"),
+            (0, 0))
         logo = Image(
-            content.load_image("game-logo.png"), (70, 80))
+            utils.load_mm_image("logo"),
+            (930, 590))
         label_version = Label(
-            "Version 1.0.0a1 - For testing purposes only.", utils.fonts["sm"], pygame.Color("white"), (35, 555))
-        label_copyright = Label(
-            "© 2022 Pacific Tech", utils.fonts["sm"], pygame.Color("white"), (35, 565))
-        self.timer = Timer(1500, True, True)
-        def update_time():
-            # label_version.set_text(str((self.timer.get_remaining())))
-            alpha = 0 + (255 * (self.timer.get_elapsed() / self.timer.interval))
-            label_copyright.set_text(str(alpha))
-            label_version.get_surface().set_alpha(alpha)
-        self.timer.on_tick = update_time
+            "Version 1.0.0a1 - For testing purposes only.",
+            utils.fonts["sm"],
+            pygame.Color("white"),
+            (20, 690))
+        logo_company = Image(
+            utils.load_mm_image("logo-company"),
+            (20, 710))
 
-        button_new_game = Button.from_button(utils.button_default, "New Game")
-        button_new_game.set_position((510, 100))
-        def on_new_game_click():
-            pass
-            #game.scenes.set_scene(
-        button_new_game.on_left_click = on_new_game_click
+        btn_new = FadeButton(utils.load_mm_image("btn-new"), (475, 665))
+        btn_new.click += lambda sender, button: game.scenes.set_scene("e1m0")
+
+        btn_load = FadeButton(utils.load_mm_image("btn-load"), (610, 595))
+
+        btn_options = FadeButton(utils.load_mm_image("btn-options"), (740, 500))
+
+        btn_credits = FadeButton(utils.load_mm_image("btn-credits"), (1105, 335))
+
+        btn_exit = FadeButton(utils.load_mm_image("btn-exit"), (1280, 280))
+        btn_exit.click += lambda sender, button: game.exit()
+
+        btn_test = Button.from_entity(utils.button_default, "Test")
+        btn_test.set_position((510, 100))
+        def on_btn_test_click(sender, button):
+            import uuid
+            btn_test.get_label().set_text(str(uuid.uuid4()))
+        btn_test.click += on_btn_test_click
 
         self.entities = {
             "background": background,
             "logo": logo,
-            "label_copyright": label_copyright,
+            "logo_company": logo_company,
             "label_version": label_version,
-            "button_new_game": button_new_game,
+            "btn_new": btn_new,
+            "btn_load": btn_load,
+            "btn_options": btn_options,
+            "btn_credits": btn_credits,
+            "btn_exit": btn_exit,
+            #"btn_test": btn_test,
         }
