f8356be68981ab0f305ede408d34f364094739a7
Francis Fajardo <francisdominic.fajardo@gmail.com>
2022-06-03 11:51:17 +0800

    Draw dialog base box surface to entity surface

--- src/app/dialog.py
+++ src/app/dialog.py
@@ -11,8 +11,8 @@ from queue import Queue
 
 RECT_DIALOG = pygame.Rect(0, 0, 768, 128)
 RECT_NAME = pygame.Rect(140, 0, 155, 38)
-RECT_MAIN = pygame.Rect(0, 38, 628, 90)
-RECT_PROFILE = pygame.Rect(0, 38, 140, 90)
+RECT_BASE = pygame.Rect(0, 38, 628, 90)
+RECT_PORTRAIT = pygame.Rect(0, 0, 128, 128)
 
 class Dialog(ClickableEntity):
     def __init__(self, emitter, position, name, text, portrait_id = None, callback = None):
@@ -30,20 +30,15 @@ class Dialog(ClickableEntity):
         self.portrait = None
         if portrait_id:
             portrait = utils.load_ca_image(portrait_id)
-            portrait_pos = (
-                position[0] + RECT_NAME.height,
-                position[1]
-            )
-            self.portrait = Image(portrait, portrait_pos)
-            self.portrait.set_size((128, 128))
-            label_main_offset = RECT_PROFILE.width
+            self.portrait = Image(portrait, RECT_PORTRAIT)
+            label_main_offset = RECT_PORTRAIT.width
             box_label_name_offset = RECT_NAME.x
 
-        box_name_pos = (
-            position[0] + box_label_name_offset,
-            position[1]
-        )
-        self.box_name = Image(utils.load_ui_image("dialog-box-nametag"), box_name_pos)
+        self.box_name = Image(utils.load_ui_image("dialog-box-nametag"), (box_label_name_offset, RECT_NAME.y))
+        self.box_name.draw(self._surface)
+        self.box_base = utils.load_ui_image("dialog-box-main")
+        self._surface.blit(self.box_base, RECT_BASE)
+
         self.label_name = Label(name, utils.get_font(28), pygame.Color("white"))
         label_name_pos = (
             position[0] + box_label_name_offset + (RECT_NAME.width / 2) - self.label_name.get_rect().width / 2,
@@ -51,17 +46,15 @@ class Dialog(ClickableEntity):
         )
         self.label_name.set_position(label_name_pos)
 
-        box_main_pos = (
-            position[0],
-            position[1] + RECT_MAIN.y
-        )
-        self.box_main = Image(utils.load_ui_image("dialog-box-main"), box_main_pos)
         self.label_main = Label(text, utils.get_font(24), pygame.Color("white"))
         label_main_pos = (
             position[0] + label_main_offset,
-            position[1] + RECT_NAME.height + (RECT_MAIN.height / 2) - self.label_main.get_rect().height / 2
+            position[1] + RECT_NAME.height + (RECT_BASE.height / 2) - self.label_main.get_rect().height / 2
         )
         self.label_main.set_position(label_main_pos)
+
+        if self.portrait:
+            self.portrait.draw(self._surface)
 
     @classmethod
     def from_entity(cls, entity, name, text):
@@ -88,13 +81,9 @@ class Dialog(ClickableEntity):
         super()._on_click(button)
 
     def draw(self, layer):
-        super().draw(layer) # TODO labels and boxes should be drawn to this surface
-        self.box_main.draw(layer)
+        super().draw(layer)
         self.label_main.draw(layer)
-        self.box_name.draw(layer)
         self.label_name.draw(layer)
-        if self.portrait:
-            self.portrait.draw(layer)
 
 class DialogSide(IntEnum):
     TOP = 1
