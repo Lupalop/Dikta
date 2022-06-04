ab408fd1af7b0e4b14c8470a7f4da6bc5a0fae45
Francis Fajardo <francisdominic.fajardo@gmail.com>
2022-06-05 02:14:30 +0800

    Update E1M1

--- src/app/scenes/e1m1.py
+++ src/app/scenes/e1m1.py
@@ -1,23 +1,32 @@
 from engine import *
 from engine.entities import *
 from app import utils
+from app.mission import Mission
+from app.dialog import DialogSide, DialogFlags
 
 import pygame
 
-class E1M1Scene(Scene):
+class E1M1Scene(Mission):
     def __init__(self):
-        super().__init__("E1M1")
-
-    def update(self, game, events):
-        super().update(game, events)
-
-    def draw(self, layer):
-        super().draw(layer)
+        super().__init__(1, 1, "Dialogue 1", DialogSide.BOTTOM)
 
     def load_content(self):
-        bg_main = Image(
-            utils.load_em_image(1, 1, "bg-main"))
+        self.emitter.add("joe", 1)
+        self.emitter.add("joe", 2, callback=lambda: game.scenes.set_scene(E1M1BScene()))
+
+        self.background.set_surface(self.get_image("bg-joe"))
+
+class E1M1BScene(Mission):
+    def __init__(self):
+        super().__init__(1, 1, "Desk - Outside", DialogSide.TOP)
+
+    def load_content(self):
+        self.emitter.add("joe", "tut1", "joe-faceright", callback=lambda: game.scenes.set_scene(""))
+
+        target1 = ClickableEntity(pygame.Rect(0, 0, 700, 320), hit_rect=True)
+        target1.click += lambda sender, state: print("test")
+        self.background.set_surface(self.get_image("bg-main"))
 
         self.entities = {
-            "bg_main": bg_main,
+            "bulletin_board": target1,
         }
