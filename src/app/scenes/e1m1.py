071d3545b68593b240490b6ed44a691b2e807b5b
Francis Fajardo <francisdominic.fajardo@gmail.com>
2022-06-07 21:40:57 +0800

    Update E1M1

--- src/app/scenes/e1m1.py
+++ src/app/scenes/e1m1.py
@@ -1,32 +1,139 @@
 from engine import *
-from app import utils
+from app import utils, scene_list
 from app.entities import *
 from app.mission import Mission
 from app.dialog import DialogSide, DialogFlags
 
 import pygame
 
+# E1M1 - Introduction
+
 class E1M1Scene(Mission):
     def __init__(self):
-        super().__init__(1, 1, "Dialogue 1", DialogSide.BOTTOM)
+        super().__init__(1, 1, "", "", DialogSide.BOTTOM)
 
     def load_content(self):
-        self.emitter.add("joe", 1)
-        self.emitter.add("joe", 2, callback=lambda: game.scenes.set_scene(E1M1BScene()))
+        super().load_content()
+        self.emitter.add("joe", "intro1", repeat = False)
+        self.emitter.add("joe", "intro2", callback=lambda: game.scenes.set_scene(E1M1DeskOutside()))
 
         self.background.set_surface(self.get_image("bg-joe"))
 
-class E1M1BScene(Mission):
+scene_list.add_mission(E1M1Scene())
+
+# E1M1 - Desk - Outside
+
+class E1M1DeskOutside(Mission):
     def __init__(self):
-        super().__init__(1, 1, "Desk - Outside", DialogSide.TOP)
+        super().__init__(1, 1, "desk_outside", "Desk - Outside", DialogSide.BOTTOM)
 
     def load_content(self):
-        self.emitter.add("joe", "tut1", "joe-faceright", callback=lambda: game.scenes.set_scene(""))
-
-        target1 = ClickableEntity(self, pygame.Rect(0, 0, 700, 320), hit_rect=True)
-        target1.click += lambda sender, state: print("test")
+        super().load_content()
         self.background.set_surface(self.get_image("bg-main"))
+        self.emitter.add("joe", "tut1", "joe-faceright")
+
+        target1 = TargetMask(self, self.get_image("tm-main-bulletinboard"))
+        target1.leftclick += lambda sender: self.emitter.add("joe", "target_board", "joe-faceright")
+
+        target2 = TargetMask(self, self.get_image("tm-main-newspapers"))
+        target2.leftclick += lambda sender: self.emitter.add("joe", "newspapers", "joe-faceright")
+
+        target3 = TargetMask(self, self.get_image("tm-main-joe"))
+        target3.leftclick += lambda sender: self.emitter.add("joe", ["joe", "tut2"], "joe-faceright")
+
+        target4 = TargetMask(self, self.get_image("tm-main-nmdesk"))
+        target4.leftclick += lambda sender: self.emitter.add("joe", "nmdesk", "joe-faceright")
+
+        target5 = TargetMask(self, self.get_image("tm-main-desk"))
+        target5.leftclick += lambda sender: game.scenes.set_scene(E1M1DeskInside())
+
+        target6 = TargetMask(self, self.get_image("tm-main-light"))
+        target6.leftclick += lambda sender: self.emitter.add("joe", ["light1", "light2"], "joe-faceright")
 
         self.entities = {
-            "bulletin_board": target1,
+            "target1": target1,
+            "target2": target2,
+            "target3": target3,
+            "target4": target4,
+            "target5": target5,
+            "target6": target6,
         }
+
+scene_list.add_mission(E1M1DeskOutside())
+
+# E1M1 - Desk - Inside (HO)
+
+class E1M1DeskInside(Mission):
+    def __init__(self):
+        super().__init__(1, 1, "desk_inside", "Desk - Inside", DialogSide.TOP)
+
+    def load_content(self):
+        super().load_content()
+        self.background.set_surface(self.get_image("bg-desk"))
+        self.emitter.add("joe", "desk", "joe-faceright", repeat = False)
+        self.entities = {}
+
+        item1 = TargetItem(self, self.get_image("item-stapler"), (263, 19), removable = False)
+        item1.leftclick += lambda sender: self.emitter.add("joe", "item_stapler", "joe-faceright")
+        self.entities["item_stapler"] = item1
+
+        if not self.exists_item("wallet"):
+            item2 = TargetItem(self, self.get_image("item-wallet"), (320, 120))
+            self.attach_item(item2, "wallet")
+            item2.leftclick += lambda sender: self.emitter.add("joe", "item_wallet", "joe-faceright")
+            self.entities["item_wallet"] = item2
+
+        item3 = TargetItem(self, self.get_image("item-reports"), (160, 130), removable = False)
+        item3.leftclick += lambda sender: self.emitter.add("joe", "item_reports", "joe-faceright")
+        self.entities["item_reports"] = item3
+
+        item4 = TargetItem(self, self.get_image("item-typewriter"), (386, 10), removable = False, grabbable = False)
+        item4.leftclick += lambda sender: self.emitter.add("joe", "item_typewriter", "joe-faceright")
+        self.entities["item_typewriter"] = item4
+
+        if not self.exists_item("press-card"):
+            item5 = TargetItem(self, self.get_image("item-press-card"), (26, 302))
+            self.attach_item(item5, "press-card")
+            item5.leftclick += lambda sender: game.scenes.set_scene("e1m1popup_presscard") # TODO
+            self.entities["item_press_card"] = item5
+
+        item6 = TargetItem(self, self.get_image("item-papers"), (916, 363), removable = False, grabbable = False)
+        item6.leftclick += lambda sender: self.emitter.add("joe", "item_papers", "joe-faceright")
+        self.entities["item_papers"] = item6
+
+        item7 = TargetItem(self, self.get_image("item-mail"), (1050, 109), removable = False, grabbable = False)
+        def item7_leftclick(sender):
+            if not self.exists_item("note"):
+                game.scenes.set_scene("e1m1popup_note") # TODO
+            else:
+                self.emitter.add("joe", "item_mail", "joe-faceright")
+        item7.leftclick += item7_leftclick
+        self.entities["item_mail"] = item7
+
+        item8 = TargetItem(self, self.get_image("item-pencils"), (994, 318), removable = False)
+        def item8_leftclick(sender):
+            if self.exists_item("pen"):
+                self.emitter.add("joe", "item_pencils_empty", "joe-faceright")
+            else:
+                self.add_item("pen")
+                self.emitter.add("joe", "item_pencils", "joe-faceright")
+        item8.leftclick += item8_leftclick
+        self.entities["item_pencils"] = item8
+
+        if not self.exists_item("journal"):
+            item9 = TargetItem(self, self.get_image("item-journal"), (1139, 601))
+            self.attach_item(item9, "journal")
+            item9.leftclick += lambda sender: self.emitter.add("joe", "item_journal", "joe-faceright")
+            self.entities["item_journal"] = item9
+
+        drawer = TargetMask(self, self.get_image("tm-desk-drawer"))
+        def drawer_leftclick(sender):
+            if self.exists_item("flashlight"):
+                self.emitter.add("joe", "drawer_empty", "joe-faceright")
+            else:
+                self.add_item("flashlight")
+                game.scenes.set_scene("e1m1popup_flashlight") # TODO
+        drawer.leftclick += drawer_leftclick
+        self.entities["drawer"] = drawer
+
+scene_list.add_mission(E1M1DeskInside())
