de7b9eb53f4bcfc82e2e2a78c02bffc1b16e7c31
Francis Fajardo <francisdominic.fajardo@gmail.com>
2022-06-10 03:41:53 +0800

    E1M2: Initial landing

--- src/app/scenes/__init__.py
+++ src/app/scenes/__init__.py
@@ -4,7 +4,7 @@ from .debug import DebugOverlay
 from .e0mtest import E0MTestScene
 from .e1m0 import E1M0Scene
 from .e1m1 import E1M1Scene
-from .e1m2 import E1M2Scene
+from . import e1m2
 from .e1m3 import E1M3Scene
 from .e1m4 import E1M4Scene
 from .e1m5 import E1M5Scene
--- src/app/scenes/e1m2.py
+++ src/app/scenes/e1m2.py
@@ -1,25 +1,112 @@
 from engine import *
-from app import utils
+from app import defaults, scene_list
 from app.entities import *
 from app.mission import Mission
 from app.dialog import DialogSide, DialogFlags
 
 import pygame
 
-class E1M2Scene(Mission):
+# E1M2 - Intermezzo
+
+class E1M2Intermezzo(Mission):
     def __init__(self):
-        super().__init__(1, 2)
+        super().__init__(1, 2, "intermezzo", "Intermezzo", DialogSide.TOP)
 
-    def update(self, game, events):
-        super().update(game, events)
+    def _next(self):
+        game.scenes.set_scene("e1m2taxi_joe")
 
-    def draw(self, layer):
-        super().draw(layer)
+    def _puppet_taximove_out(self):
+        self.animator.to_position(
+            self.entities["taxi"],
+            10000,
+            1500,
+            delta=True,
+            callback=self._next,
+            callback_delay=500
+        )
+        self.animator.to_position(
+            self.entities["taxi_driver"],
+            10000,
+            1500,
+            delta=True
+        )
+        self.animator.to_position(
+            self.entities["taxi_joe"],
+            10000,
+            1500,
+            delta=True
+        )
+
+    def _puppet_entertaxi(self):
+        self.entities.pop("joe_standing")
+        self.entities["taxi_joe"].get_surface().set_alpha(255)
+        delay_timer = self.timers.add(500, True)
+        delay_timer.elapsed += lambda sender: self._puppet_taximove_out()
+
+    def _puppet_taximove_in(self):
+        self.animator.to_position(
+            self.entities["taxi"],
+            10000,
+            -10,
+            callback=self._puppet_entertaxi,
+        )
+        self.animator.to_position(
+            self.entities["taxi_driver"],
+            10000,
+            615,
+        )
 
     def load_content(self):
-        bg_main = Image(
-            utils.load_em_image(1, 1, "bg-main"))
+        super().load_content()
+        self.background.set_surface(self.get_image("outside-bg"))
+        self.emitter.add(
+            "joe",
+            "call_taxi",
+            "joe-faceright",
+            callback=self._puppet_taximove_in
+        )
+
+        joe_standing = Image(self, self.get_image("outside-prop-joe-waiting"), (540, 265))
+        taxi_driver = Image(self, self.get_image("outside-prop-driver"), (-735, 317))
+        taxi_joe = Image(self, self.get_image("outside-prop-joe"), (458, 323))
+        taxi_joe.get_surface().set_alpha(0)
+        taxi = Image(self, self.get_image("outside-prop-taxi"), (-1360, 105))
 
         self.entities = {
-            "bg_main": bg_main,
+            "joe_standing": joe_standing,
+            "taxi_driver": taxi_driver,
+            "taxi_joe": taxi_joe,
+            "taxi": taxi,
         }
+
+scene_list.add_mission(E1M2Intermezzo())
+
+# E1M2 - Taxi - Inside (Joe)
+
+class E1M2TaxiInsideJoe(Mission):
+    def __init__(self):
+        super().__init__(1, 2, "taxi_joe", "Taxi - Inside (Joe)", DialogSide.BOTTOM_LEFT)
+
+    def load_content(self):
+        super().load_content()
+        self.background.set_surface(self.get_image("taxi-bg-joe"))
+        self.emitter.add(
+            "joe",
+            "initial_question",
+            #callback=self._puppet_taximove_in
+        )
+"""
+        joe_standing = Image(self, self.get_image("outside-prop-joe-waiting"), (540, 265))
+        taxi_driver = Image(self, self.get_image("outside-prop-driver"), (-735, 317))
+        taxi_joe = Image(self, self.get_image("outside-prop-joe"), (458, 323))
+        taxi_joe.get_surface().set_alpha(0)
+        taxi = Image(self, self.get_image("outside-prop-taxi"), (-1360, 105))
+
+        self.entities = {
+            "joe_standing": joe_standing,
+            "taxi_driver": taxi_driver,
+            "taxi_joe": taxi_joe,
+            "taxi": taxi,
+        }
+"""
+scene_list.add_mission(E1M2TaxiInsideJoe())
