c3edc0fae399718e36577a448ece63a026126831
Francis Fajardo <francisdominic.fajardo@gmail.com>
2022-06-10 23:44:14 +0800

    E1M2: Implement most of the Joe-Driver dialogue

--- src/app/scenes/e1m2.py
+++ src/app/scenes/e1m2.py
@@ -88,13 +88,22 @@ class E1M2TaxiInsideJoe(Mission):
     def __init__(self):
         super().__init__(1, 2, "taxi_joe", "Taxi - Inside (Joe)", DialogSide.BOTTOM_LEFT)
 
+    def _to_driver(self):
+        game.scenes.set_scene("e1m2taxi_driver")
+
     def _choice_handle(self, sender, value):
-        # Choice 0: Leave
-        if value[0] == 0:
-            game.scenes.set_scene("e1m2intermezzo")
-        # Choice 1: Stay
-        else:
-            self.emitter.add("joe", "return")
+        i = value[0]
+        self.set_switch("taxi_choice", i)
+        # Choice 0: Luneta Park
+        if i == 0:
+            self.emitter.add("joe", "choice1", callback=self._to_driver)
+        # Choice 1: Congress
+        elif i == 1:
+            self.emitter.add("joe", "choice2", callback=self._to_driver)
+        # Choice 2: Home
+        elif i == 2:
+            self.emitter.add("joe", "choice3_1")
+            self.emitter.add("joe", "choice3_2", callback=self._to_driver)
         self.emitter.next()
 
     def _choice_create(self, sender):
@@ -105,24 +114,62 @@ class E1M2TaxiInsideJoe(Mission):
     def load_content(self):
         super().load_content()
         self.background.set_surface(self.get_image("taxi-bg-joe"))
-        initial_dialog = self.emitter.add(
-            "joe",
-            "initial_question",
-            flags=DialogFlags.SKIPPABLE
-        )
-        initial_dialog.label_speech.completed += self._choice_create
-"""
-        joe_standing = Image(self, self.get_image("outside-prop-joe-waiting"), (540, 265))
-        taxi_driver = Image(self, self.get_image("outside-prop-driver"), (-735, 317))
-        taxi_joe = Image(self, self.get_image("outside-prop-joe"), (458, 323))
-        taxi_joe.get_surface().set_alpha(0)
-        taxi = Image(self, self.get_image("outside-prop-taxi"), (-1360, 105))
 
-        self.entities = {
-            "joe_standing": joe_standing,
-            "taxi_driver": taxi_driver,
-            "taxi_joe": taxi_joe,
-            "taxi": taxi,
-        }
-"""
+        taxi_choice = self.find_switch("taxi_choice")
+        if not taxi_choice:
+            initial_dialog = self.emitter.add(
+                "joe",
+                "choice_initial",
+                flags=DialogFlags.SKIPPABLE
+            )
+            initial_dialog.label_speech.completed += self._choice_create
+            return
+
+        taxi_pre_reset = self.find_switch("taxi_pre_reset")
+        if taxi_pre_reset:
+            self.emitter.add("joe", "choice3_4", callback=self._to_driver)
+            self.set_switch("taxi_reset", True)
+            self.clear_switch("taxi_choice")
+            self.clear_switch("taxi_pre_reset")
+
 scene_list.add_mission(E1M2TaxiInsideJoe())
+
+# E1M2 - Taxi - Inside (Driver)
+
+class E1M2TaxiInsideDriver(Mission):
+    def __init__(self):
+        super().__init__(1, 2, "taxi_driver", "Taxi - Inside (Driver)", DialogSide.BOTTOM_RIGHT)
+
+    def _to_pay(self):
+        self.set_switch("taxi_pay", True)
+        game.scenes.set_scene("e1m2taxi_driver")
+
+    def _to_joe(self):
+        game.scenes.set_scene("e1m2taxi_joe")
+
+    def load_content(self):
+        super().load_content()
+        self.background.set_surface(self.get_image("taxi-bg-driver"))
+
+        taxi_pay = self.find_switch("taxi_pay")
+        if taxi_pay:
+            self.emitter.add("driver", "pay1", "taxidriver-main", callback=self._to_joe)
+            return
+
+        taxi_reset = self.find_switch("taxi_reset")
+        if taxi_reset:
+            self.emitter.add("driver", "choice3_5", "taxidriver-main", callback=self._to_joe)
+            self.clear_switch("taxi_reset")
+            return
+
+        taxi_choice = self.find_switch("taxi_choice")
+        if taxi_choice:
+            if taxi_choice == 0:
+                self.emitter.add("driver", "choice1", "taxidriver-main", callback=self._to_pay)
+            elif taxi_choice == 1:
+                self.emitter.add("driver", "choice2", "taxidriver-main", callback=self._to_pay)
+            elif taxi_choice == 2:
+                self.emitter.add("driver", "choice3_3", "taxidriver-main", callback=self._to_joe)
+                self.set_switch("taxi_pre_reset", True)
+
+scene_list.add_mission(E1M2TaxiInsideDriver())
