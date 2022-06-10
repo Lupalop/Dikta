c962d52a8ff60e1ed29b486dfeee3cb1fb3e2b29
Francis Fajardo <francisdominic.fajardo@gmail.com>
2022-06-11 00:31:01 +0800

    Separate tests into their own function

--- src/app/scenes/e0mtest.py
+++ src/app/scenes/e0mtest.py
@@ -12,13 +12,24 @@ class E0MTestScene(Mission):
 
     def update(self, game, events):
         super().update(game, events)
+        for event in events:
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_F1:
+                    self._test_dialog()
+                elif event.key == pygame.K_F2:
+                    self._test_choices()
+                elif event.key == pygame.K_F3:
+                    pass
+                elif event.key == pygame.K_F4:
+                    pass
+                elif event.key == pygame.K_F5:
+                    pass
+                break
 
     def draw(self, layer):
         super().draw(layer)
 
-    def load_content(self):
-        super().load_content()
-
+    def _test_dialog(self):
         self.emitter.add("joe", 1, callback=lambda:print("test"))
         self.emitter.add("joe", 2, "joe-faceright", flags=DialogFlags.CLOSEABLE)
         self.emitter.add("joe", 3)
@@ -40,20 +51,23 @@ class E0MTestScene(Mission):
             self,
             utils.load_em_image(1, 1, "bg-main"))
 
+        self.entities = {
+            "bg_main": bg_main,
+        }
+
+    def _test_choices(self):
         choice1 = ChoiceButton(
             self,
             (50, 50),
             1,
             "Luneta Park"
         )
-
         choice2 = ChoiceButton(
             self,
             (50, 125),
             2,
             "Congress"
         )
-
         choice3 = ChoiceButton(
             self,
             (50, 200),
@@ -66,7 +80,6 @@ class E0MTestScene(Mission):
             (350, 150),
             ["Luneta Park", "Congress", "Home"]
         )
-
         choiceset2 = ChoiceSet(
             self,
             (650, 150),
@@ -75,12 +88,14 @@ class E0MTestScene(Mission):
         )
 
         self.entities = {
-            # "bg_main": bg_main,
             "choice1": choice1,
             "choice2": choice2,
             "choice3": choice3,
             "choiceset": choiceset,
             "choiceset2": choiceset2
         }
+
+    def load_content(self):
+        super().load_content()
 
 scene_list.add_mission(E0MTestScene())
