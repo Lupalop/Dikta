e4daf1f6980c13bb50e391bac2f4716a8ffd3825
Francis Fajardo <francisdominic.fajardo@gmail.com>
2022-06-12 12:54:07 +0800

    Use autoload for scene modules

--- src/app/scenes/__init__.py
+++ src/app/scenes/__init__.py
@@ -1,18 +1,4 @@
-from .mouse import MouseOverlay
-from .debug import DebugOverlay
+from autoload import ModuleLoader
+loader = ModuleLoader()
 
-from .e0mtest import E0MTestScene
-from .e1m0 import E1M0Scene
-from .e1m1 import E1M1Scene
-from . import e1m2
-from .e1m3 import E1M3Scene
-from .e1m4 import E1M4Scene
-from .e1m5 import E1M5Scene
-from .e1m6 import E1M6Scene
-from .e1m7 import E1M7Scene
-from .e1m8 import E1M8Scene
-from .e1m9 import E1M9Scene
-
-from .main_menu import MainMenuScene
-from .ingame_escmenu import InGameEscMenuOverlay
-from .ingame_items import InGameItemsOverlay
+ALL = loader.load_classes("")
