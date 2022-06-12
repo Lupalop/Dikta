b574ea2390edb08f2f80b9c55342d2a36d86962b
Francis Fajardo <francisdominic.fajardo@gmail.com>
2022-06-12 16:04:42 +0800

    E1M3: Initial set-up

--- src/app/scenes/e1m3.py
+++ src/app/scenes/e1m3.py
@@ -1,5 +1,5 @@
 from engine import *
-from app import utils
+from app import defaults, scene_list
 from app.entities import *
 from app.mission import Mission
 from app.dialog import DialogSide, DialogFlags
@@ -8,7 +8,7 @@ import pygame
 
 class E1M3Scene(Mission):
     def __init__(self):
-        super().__init__(1, 3)
+        super().__init__(1, 3, "", "Congress - Outside", DialogSide.TOP)
 
     def update(self, game, events):
         super().update(game, events)
@@ -17,9 +17,11 @@ class E1M3Scene(Mission):
         super().draw(layer)
 
     def load_content(self):
-        bg_main = Image(
-            utils.load_em_image(1, 1, "bg-main"))
+        super().load_content()
+        self.background.set_surface(self.get_image("main-bg"))
 
         self.entities = {
-            "bg_main": bg_main,
+            #"bg_main": bg_main,
         }
+
+scene_list.add_mission(E1M3Scene())
