582c1a8fb8c7489ad54cbae7c4332cab228fadbd
Francis Dominic Fajardo <fajardo@duck.com>
2026-03-14 01:03:06 +0800

    Use importlib instead of autoload

--- src/app/scenes/__init__.py
+++ src/app/scenes/__init__.py
@@ -1,4 +1,7 @@
-from autoload import ModuleLoader
-loader = ModuleLoader()
+import importlib
+import pkgutil
 
-ALL = loader.load_classes("")
+# Dynamically import all modules in this package to register scenes.
+for loader, module_name, is_pkg in pkgutil.walk_packages(__path__):
+    if not is_pkg:
+        importlib.import_module(f".{module_name}", __package__)
